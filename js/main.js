0e71dbe8ad602917417c71546adfc6da7dd33ef3
lev tarasov <56757711+levil664@users.noreply.github.com>
2022-09-19 10:56:14 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
  * @param {number} max - максимальное число диапазона
  * @returns {number} - случайное число из диапазона
  */
-function calculateRandomNumber(min, max) {
+ function calculateRandomNumber(min, max) {
   if (min < 0 || min >= max) {
     throw {name : 'Invalid arguments', message : 'you passed invalid arguments'};
   }
@@ -17,16 +17,10 @@ calculateRandomNumber(12, 43);
 /**
  * Функция для проверки максимальной длины строки.
  *
- * @param {object} verifiableString - проверяемая строка
+ * @param {string} verifiableString - проверяемая строка
  * @param {number} maxLenght - максимальная длина строки
  * @returns {boolean} - true, если строка проходит по длине, и false — если не проходит
  */
-function сheckLengthOfString(verifiableString, maxLenght) {
-  verifiableString = String(verifiableString);
-  if (verifiableString.length < maxLenght) {
-    return true;
-  }
-  return false;
-}
+const сheckLengthOfString = (verifiableString, maxLenght) => verifiableString.length < maxLenght;
 
 сheckLengthOfString('zxcqweasdqweasdzxc', 122);
