319f01bb281dc323a33cee8d0c9a017f16873bbf
lev tarasov <56757711+levil664@users.noreply.github.com>
2022-10-03 17:28:07 +0500

    Continue to work on the Kekstagram

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+/* eslint-disable no-unused-vars */
+// Подключаем файл с набор данных
+const dataSet = document.createElement('dataSet');
+// Задаем количество фотографий и диапазон лайков
+const quantityPictures = 25;
+const likes = {
+  min: 15,
+  max: 200,
+};
+dataSet.src = '/dataSet.js';
+document.head.appendChild(dataSet);
+
+
 /**
  * Функция, возвращающая случайное целое число из переданного диапазона включительно.
  *
@@ -12,7 +25,6 @@ function calculateRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-calculateRandomNumber(12, 43);
 
 /**
  * Функция для проверки максимальной длины строки.
@@ -23,4 +35,58 @@ calculateRandomNumber(12, 43);
  */
 const сheckLengthOfString = (verifiableString, maxLenght) => verifiableString.length < maxLenght;
 
-сheckLengthOfString('zxcqweasdqweasdzxc', 122);
+
+/**
+ * Функция, возвращающая случайный элемент из переданного массива.
+ *
+ * @param {object} array - массив из которого нужно выбрать элемент
+ * @returns - случайный элемент из массива
+ */
+const getRandomElement = (array) => array[calculateRandomNumber(0, array.length - 1)];
+
+
+/**
+ * Функция, создающая комментарий к фотографии.
+ *
+ * @param {number} id - случайное число. Идентификаторы не должны повторяться.
+ * @returns {object} - комментарий в виде объекта.
+ */
+function createComment(id){
+  return {
+    id: calculateRandomNumber(1, 500),
+    avatar: `img/avatar-${id}.svg`,
+    message: getRandomElement(dataSet.COMMENTS),
+    name: getRandomElement(dataSet.NAMES)
+  };
+}
+
+
+/**
+ * Функция, создающая комментарий к фотографии.
+ *
+ * @param {number} len - длинна создаваемого массива(пустого).
+ * @param {function} func - функция, которая будет заполнять массив.
+ * @returns {array}
+ */
+const createArrayObj = (len, func) => Array.from({length: len}).map((value, index) => func(index + 1));
+
+
+// Массив из quantityPictures сгенерированных объектов.
+const resultArray = createArrayObj(quantityPictures, createDescription);
+
+
+/**
+ * Функция, создающая комментарий к фотографии.
+ *
+ * @param {number} id - случайное число. Идентификаторы не должны повторяться.
+ * @returns {object} - информация о фотографии.
+ */
+function createDescription(id){
+  return {
+    id: id,
+    url: `photos/${id}.jpg`,
+    description: getRandomElement(dataSet.DESCRIPTION),
+    likes: calculateRandomNumber(likes.min, likes.max),
+    comments: createArrayObj(getRandomElement(1, 6), createComment),
+  };
+}
