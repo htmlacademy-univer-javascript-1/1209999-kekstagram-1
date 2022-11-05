d92fdee057d9f82c40adfb9f16b52f3e2710d1c9
lev tarasov <56757711+levil664@users.noreply.github.com>
2022-11-05 17:37:45 +0500

    some changes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 /* eslint-disable no-unused-vars */
 import './util.js';
 import './data.js';
-import {resultRender} from './rendering-pictures.js';
+import './rendering-pictures.js';
 
-
-// eslint-disable-next-line no-console
-console.log(resultRender);
